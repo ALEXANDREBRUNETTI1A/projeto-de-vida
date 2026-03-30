7dc75a86f43565868ef4b751e3380cce7d08067c
ALEXANDREBRUNETTI1A <165664490+ALEXANDREBRUNETTI1A@users.noreply.github.com>
2026-03-30 12:32:29 -0300

    s

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
-    
-    // Remove a classe "ativo" de todos os botões e textos
     for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
-      textos[j].classList.remove("ativo");
     }
 
-    // Adiciona a classe "ativo" apenas no item clicado (índice i)
     botoes[i].classList.add("ativo");
-    textos[i].classList.add("ativo");
   };
+}
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        // Remove a classe 'ativo' de todos os botões e textos
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+
+        // Adiciona a classe 'ativo' apenas no que foi clicado
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
 }
