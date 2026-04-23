a2e17a79c46168bf587dbfc095462822783b4bc6
ALEXANDREBRUNETTI1A <165664490+ALEXANDREBRUNETTI1A@users.noreply.github.com>
2026-04-23 09:38:55 -0300

    s

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-//aula 2
+//aula 5 (substituir a aula 4)
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
-botoes[i].onclick = function(){
+for (let i = 0; i < botoes.length; i++) {
+botoes[i].onclick = function () {
 
-for(let j=0;j<botoes.length;j++){
+for (let j = 0; j < botoes.length; j++) {
 botoes[j].classList.remove("ativo");
+textos[j].classList.remove("ativo");
 }
 
 botoes[i].classList.add("ativo");
+textos[i].classList.add("ativo");
 }
 }
-//fim aula2
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-04-20T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
