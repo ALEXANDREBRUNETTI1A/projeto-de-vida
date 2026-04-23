b3cd5b330a5a09812c3ae565c6c01f64ac999922
ALEXANDREBRUNETTI1A <165664490+ALEXANDREBRUNETTI1A@users.noreply.github.com>
2026-04-23 09:36:47 -0300

    s

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-c//aula 2
+//aula 2
 const botoes = document.querySelectorAll(".botao");
 
-for (let i = 0; i < botoes.length; i++) {
-botoes[i].onclick = function () {
+for(let i=0;i <botoes.length;i++){
+botoes[i].onclick = function(){
 
-for (let j =0;j<botoes.length;j++) {
-
+for(let j=0;j<botoes.length;j++){
 botoes[j].classList.remove("ativo");
-
 }
+
 botoes[i].classList.add("ativo");
 }
 }
